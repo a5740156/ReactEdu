7a909083aa3cf072785956513fe451945542d309
a5740156 <a5740156a@foresys.co.kr>
2023-05-31 09:51:57 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button,Navbar, Container, Nav } from 'react-bootstrap';
+import timeTable from './img/시차표.png';
+import { useState } from 'react';
+import sData from './data.js';
 
 function App() {
+
+  let[shoes] = useState(sData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">EGG</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Cart</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{backgroundImage : 'url('+ timeTable + ')' }}>
+      </div>
+      <div className='container'>
+
+
+        <div className='row'>
+          {
+            sData.map( (a,i) =>  {
+              return (
+                <Card shoes={sData[i]} i={i}/>
+              )
+            })
+          }
+          
+          
+         
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return(
+    <div className='col-md-4'>
+      <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) + '.jpg'}  width="80%"/>
+      <h2>{props.shoes.title}</h2>
+      <p>{props.shoes.content}</p>
     </div>
   );
 }
